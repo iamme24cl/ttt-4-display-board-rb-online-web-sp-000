30128f76fa0717de29878cbfd2c09a76b13efcde
Chandra Lama <iamme24cl@gmail.com>
2020-09-09 03:28:14 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -177,6 +177,7 @@ describe "#display_board in 'lib/display_board.rb" do
     end
 
     it 'prints an entire board full of Os' do
+      
       # Can you copy the syntax of the tests above to write a test for a board
       # entirely filled with Os?
 
@@ -185,7 +186,7 @@ describe "#display_board in 'lib/display_board.rb" do
       # example's situation.
 
       # *** Comment the line below by adding a # at the line start ***
-      expect(true).to be(true)
+      #expect(true).to be(true)
     end
   end
 end
